5446ec6ef607edc15322920beb19ead701e58bef
Rachael Cronin <rlouise@email.unc.edu>
2022-01-30 17:19:14 -0500

    Progress on EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -19,18 +19,22 @@ while index < len(secret_word):
         # the equalizing statement between guess_input[index] and secret_word[index] checks to see if characters in guess_input at the current index are the same as the characters in secret_word in the same index, which hints to players if the characters in the guesses are in the correct location.
         # when the characters in the indices match, a str representing the green emoji is added to the empty result_guess str to visually signal players that characters of their guess are in the correct index.
     else:
-        any_matches = False
+        any_matches = False 
         alt_indices: int = 0
-        while any_matches = not True and index < len(secret_word): 
+        #creating a variable to track the other indices we are checking after confirming that the same indices between guess_input and secret_word do not match, this is so we can see if certain characters in guess_input exist at any indices in secret_word at all.
+        while any_matches not True and index < len(secret_word):
             if secret_word[alt_indices] == guess_input[index]:
                 any_matches = True
+                #if character at the alternate index in guess_input are found at the current index in secret_word, then any_matches becomes true
             else:
                 alt_indices += 1
+                #if the character in a specific index does not match the character in the current index of the secret_word, then the alt_indices is increased by one so the process can be repeated at another index until all indices have been checked for matches and the loop ends.
         if any_matches = True:
             result_guess +=  YELLOW_BOX
+            #if character at alternate indices of the guess_input are found in the indices of the secret_word, but not the same indices, then a yellow box emoji is added to result str to relay this to the player
         else:
             result_guess += WHITE_BOX
-        # where the indices of the guess_input and secret_word do not match, a str representing the white box emoji is added to the result_guess empty str
+        # when none of the characters at any of the indices of the guess_input are found at any of the indices of the secret_word, a str representing the white box emoji is added to the result_guess empty str
     index += 1
     # increasing the index numerical value ensures the computer is not evaluating the same index forever, the computer circles back through the loop until the index number is equal to the length of the word, meaning there are no more characters to check, and the loop ends.
 print(result_guess)
