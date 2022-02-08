869fb26d1178f4eaaa2ee3cab804f16b452443a6
Rachael Cronin <rlouise@email.unc.edu>
2022-02-07 21:11:24 -0500

    Progress on Exercise 3

---
+++ lessons/love_functions.py
@@ -0,0 +1,21 @@
+"""Some examples of tender, loving function definitions."""
+
+
+def love(name: str) -> str:
+    """Given a name as a parameter, returns a loving string."""
+    return (f"I love you {name}!")
+
+
+print(love("Mom"))
+
+
+def spread_love(to: str, n: int) -> str:
+    """Generates a string that repeats a loving message n times."""
+    love_note: str = ""
+    i: int = 0
+    while i < n:
+        # concatenation
+        love_note += love(to) + "\n"
+        i += 1
+    return love_note
+    
