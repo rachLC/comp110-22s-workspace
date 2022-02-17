a119d703ff835a651abfe85f5686f78f5e85ea89
Rachael Cronin <rlouise@email.unc.edu>
2022-02-16 21:47:45 -0500

    Progress on Exercise 4

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -1,4 +1,4 @@
-"""EX04 - Turtle: Draw a scene from pacman."""
+"""EX04 - Turtle: Draw pacman about to eat some ghosts and cherries."""
 
 __author__ = "730394362"
 
@@ -7,54 +7,49 @@ from turtle import Turtle, done
 
 def main() -> None:
     """The entrypoint of my scene."""
-    # TODO: Declare your Turtle variable(s) here.
+    # Declaring and establishing turtle variable.
     raphael: Turtle = Turtle()
-    # TODO: Call the procedures you define and pass your Turtle(s) as an argument.
     raphael.up()
     raphael.goto(400, -400.0)
-    raphael.down()
-    raphael.left(90)
+    # Calling function that will create a black square for the background.
     draw_outline(raphael, 300.0, -300.0)
     raphael.up()
     raphael.goto(0.0, 250.0)
-    raphael.down()
-    raphael.right(180)
+    # Calling function that will create pacman shape and color it.
     draw_pacman(raphael, 0.0, 250.0)
-    raphael.up()
     n: float = 200.0
+    # Creating loop in order to produce 3 white squares without having to keep calling the function.
     while n > 50.0:
         raphael.goto(0.0, n)
-        raphael.down()
         draw_squares(raphael, 0.0, n)
-        raphael.up()
         n -= 50.0
     raphael.goto(0.0, 0.0)
-    raphael.down()
+    # Assigning a color to the first ghost.
     raphael.color("pink")
     raphael.begin_fill()
+    # Calling function that will creates the ghost shape.
     draw_ghost(raphael, 0.0, 0.0)
     raphael.end_fill()
-    raphael.up()
     raphael.goto(0.0, -100.0)
-    raphael.down()
     raphael.left(130)
+    # Assigning color to the second ghost.
     raphael.color("blue")
     raphael.begin_fill()
+    # Calling function that draws ghost shape again to create a second ghost.
     draw_ghost(raphael, 0.0, -100.0)
     raphael.end_fill()
-    raphael.up()
     raphael.goto(0.0, -200.0)
-    raphael.down()
     raphael.right(250)
+    # Calling function that will draw and color cherries.
     draw_cherries(raphael, 0.0, -200.0)
     done()
 
 
-# TODO: Define the procedures for other components in your scene here.
-
 def draw_outline(raphael: Turtle, x: float, y: float) -> None:
     """Draws a box to outline the picture and provide a background."""
     i: int = 0
+    raphael.down()
+    raphael.left(90)
     raphael.color("black")
     raphael.begin_fill()
     while i < 5:
@@ -62,10 +57,13 @@ def draw_outline(raphael: Turtle, x: float, y: float) -> None:
         raphael.left(90)
         i += 1
     raphael.end_fill()
+    raphael.up()
 
 
 def draw_pacman(raphael: Turtle, x: float, y: float) -> None:
-    """Draws circle with triangle missing to make pacman."""
+    """Draws incomplete circle with two lines coming from center to make pacman with an open mouth, and lined and filled in with yellow."""
+    raphael.down()
+    raphael.right(180)
     raphael.color("yellow")
     raphael.begin_fill()
     raphael.circle(40.0, 305.0)
@@ -74,22 +72,26 @@ def draw_pacman(raphael: Turtle, x: float, y: float) -> None:
     raphael.right(120)
     raphael.forward(40)
     raphael.end_fill()
+    raphael.up()
 
 
 def draw_squares(raphael: Turtle, x: float, y: float) -> None:
-    """Draws square dots that pacman follows."""
+    """Draws white square dots that pacman follows."""
     i: int = 0
     raphael.color("white")
     raphael.begin_fill()
     while i < 5:
+        raphael.down()
         raphael.forward(10)
         raphael.right(90)
+        raphael.up()
         i += 1
     raphael.end_fill()
 
 
 def draw_ghost(raphael: Turtle, x: float, y: float) -> None:
     """Draws semicircle, two parallel lines, and 4 lines to make ghost."""
+    raphael.down()
     raphael.forward(20)
     raphael.circle(30, 180)
     raphael.forward(20)
@@ -101,10 +103,12 @@ def draw_ghost(raphael: Turtle, x: float, y: float) -> None:
     raphael.forward(20)
     raphael.right(95)
     raphael.forward(20)
+    raphael.up()
 
 
 def draw_cherries(raphael: Turtle, x: float, y: float) -> None:
-    """Draws two circles and two lines to make cherries."""
+    """Draws two circles and two connected lines to make cherries."""
+    raphael.down()
     raphael.begin_fill()
     raphael.color("red")
     raphael.circle(20)
@@ -117,6 +121,7 @@ def draw_cherries(raphael: Turtle, x: float, y: float) -> None:
     raphael.begin_fill()
     raphael.circle(20)
     raphael.end_fill()
+    raphael.up()
 
 
 if __name__ == "__main__":
