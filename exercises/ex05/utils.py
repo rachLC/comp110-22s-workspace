5877623f922b6f6383727af750299d332b457908
Rachael Cronin <rlouise@email.unc.edu>
2022-02-22 13:05:23 -0500

    Lesson and Progress on Exercise 5

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -9,15 +9,26 @@ def only_evens(min: int, max: int) -> list[int]:
     i: int = (min // 2) * 2
     while i <= max:
         xs.append(i)
-        i += 1 
-    return xs
+        i += 2 
+        return xs
 
 
 
 
-def sub(a_list: list, start: int, end: int):
+def sub(a_list: list, start: int, end: int) -> list[int]:
+    """Create a subset list of a given list."""
+    a_list: list[int] = list()
 
 
 
 
-def concat( first_list: list, second_list: list):
+
+def concat(first_list: list, second_list: list) -> list[int]:
+    """Concatenate two given lists into one."""
+    full_list: str = ""
+    first: str = first_list
+    second: str = second_list
+    full_list += first  
+    full_list += second
+    return full_list
+
