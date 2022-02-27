2f4294b3611e480a9baae5a5eb1275c25f4be9a8
Rachael Cronin <rlouise@email.unc.edu>
2022-02-27 17:00:33 -0500

    Progress on Exercise 6

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -9,10 +9,17 @@ def invert(given_dict: dict[str, str]) -> dict[str, str]:
 
 
 def favorite_color(colors: dict[str, str]) -> str:
-    most_color: str = 
+    most_color: str = " "
+    for 
     return most_color
 
 
 def count(input: list[str]) -> dict[str, int]:
     result_dict: dict[]
+    for i in input:
+        if  in input:
+            += 1
+        else:
+            result_dict[ : 1]
+
     return result_dict
